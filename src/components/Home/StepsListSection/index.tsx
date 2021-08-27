afbb467c3e1fc4839a6d12ee232f557e557b9e66
Beicker Zambrano <beicker998@gmail.com>
2021-08-27 09:52:50 -0500

    feat:add task to the steps list
    Now i can add tasks or steps to my steps list. It's a feature i need to
    improve with animations and validations, but now it's working as
    expected

--- src/components/Home/StepsListSection/index.tsx
+++ src/components/Home/StepsListSection/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, Fragment } from "react";
 import { TodoWrapper, TodoTaks, TodoTitle, TodoBody } from "./styles";
 
 interface SetpsListSectionProps {
@@ -14,8 +14,13 @@ interface SetpsListSectionProps {
     Input: (props: {
       placeholder: string;
       onChange?: (event: React.KeyboardEvent) => void;
+      onKeyPress?: (event: React.KeyboardEvent) => void;
     }) => JSX.Element;
   };
+}
+
+interface IStepsState {
+  name: string;
 }
 
 export default function SetpsListSection({
@@ -24,6 +29,8 @@ export default function SetpsListSection({
   styles,
 }: SetpsListSectionProps): JSX.Element {
   const [unmountAnimation, setUnmountAnimation] = useState(false);
+  const [steps, setSteps] = useState<IStepsState[]>([]);
+  const [value, setValue] = useState("");
 
   const { Wrapper, Title, Input: TextField } = styles;
   return (
@@ -34,15 +41,35 @@ export default function SetpsListSection({
       }
     >
       <Title>What are your steps?</Title>
-      <TextField placeholder="Type here your next step" />
+      <TextField
+        placeholder="Type here your next step"
+        onChange={(event: React.KeyboardEvent) => {
+          const value = (event.target as HTMLInputElement).value;
+          setValue(value);
+        }}
+        onKeyPress={(event: React.KeyboardEvent) => {
+          if (event.key === "Enter") {
+            setSteps([...steps, { name: value }]);
+            (event.target as HTMLInputElement).value = "";
+          }
+        }}
+      />
 
       {/*TODO:Create the List component*/}
       <TodoWrapper>
         <TodoTitle>{recipeTitle}</TodoTitle>
         <TodoBody>
-          <TodoTaks>sefnvsjf</TodoTaks>
-          <TodoTaks>sefnvsjf</TodoTaks>
-          <TodoTaks>sefnvsjf</TodoTaks>
+          {steps.length < 1 ? (
+            <div>
+              <h5>No hay items</h5>
+            </div>
+          ) : (
+            steps.map((step, index) => (
+              <Fragment key={`${index}`}>
+                <TodoTaks>{step.name}</TodoTaks>
+              </Fragment>
+            ))
+          )}
         </TodoBody>
       </TodoWrapper>
     </Wrapper>
